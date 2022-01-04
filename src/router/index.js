3907804a67145ddc3e842280ca391412e9f65ba5
Guillaume <guillaume.villot@edu.univ-fcomte.fr>
2022-01-04 15:48:08 +0100

    Home V1

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/test',
+    name: 'Test',
+    component: Home
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <h1>Personnalise et précommande ta paire !</h1> 
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
+
 </script>
