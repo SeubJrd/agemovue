5dabb38826acdc061192bbb11850a481702d8a4e
Sébastien Jourdain <sebj411@gmail.com>
2022-01-05 11:59:34 +0100

    ajout svg sprite et typography.scss

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { svgSpritePlugin } from 'vue-svg-sprite'
 
 Vue.config.productionTip = false
+
+Vue.use(svgSpritePlugin, {})
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
+
