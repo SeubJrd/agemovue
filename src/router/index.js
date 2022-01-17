187435ba0e650c1182e56765b78bc059a0f69638
Sébastien Jourdain <sebj411@gmail.com>
2022-01-17 09:55:13 +0100

    Ajout du formulaire type en lien avec l'api

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Collaborations from '../views/Collaborations.vue'
+import Registration from '../views/Registration.vue'
 
 Vue.use(VueRouter)
 
@@ -28,6 +29,11 @@ const routes = [
     path: '/collaborations',
     name: 'Collaborations',
     component: Collaborations
+  },
+  {
+    path: '/registration',
+    name: 'Registration',
+    component: Registration
   }
 ]
 
---
+++ src/views/Registration.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <form @submit="submit">
+            <div>
+                <label for="username">Nom utilisateur</label>
+                <input id="username" type="text" v-model="form.username">
+            </div>
+            <div>
+                <label for="email">Email</label>
+                <input type="email" id="email" v-model="form.email">
+            </div>
+            <div>
+                <label for="password">Mot de passe</label>
+                <input type="password" id="password" v-model="form.password">
+            </div>
+ 
+            <input type="submit" value="submit">
+        </form>
+ 
+        <div v-if="success">
+            <p style="color: green;">Votre inscription est réussi !</p>
+        </div>
+ 
+        <div v-if="error">
+            <p style="color: red;">{{ errorMessage }}</p>
+        </div>
+    </div>
+</template>
+ 
+<script>
+import axios from "axios"
+export default {
+    data() {
+        return {
+            form: {
+                username: null,
+                email: null,
+                password: null,
+            },
+            success: false,
+            error: false,
+            errorMessage: null
+        }
+    },
+    methods: {
+        submit(event) {
+            event.preventDefault()
+ 
+            axios.post('https://agemovue.sebastienjourdain.com/wp-json/wp/v2/users/register', {
+                username: this.form.username,
+                email: this.form.email,
+                password: this.form.password
+            }).then(response => {
+                if (response.status === 200) {
+                    this.success = true
+                    this.error = false
+                }
+            }).catch(error => {
+                console.log('Error LOG : ', error.response)
+                this.errorMessage = error.response.data.message
+                this.error = true
+                this.success = false
+            })
+        }
+ 
+ 
+    }
+}
+</script>
+ 
+<style lang="scss">
+form {
+    text-align: left;
+    width: 980px;
+    margin: auto;
+ 
+    div {
+        display: flex;
+        margin-bottom: 15px;
+ 
+        label {
+            width: 200px;
+        }
+ 
+        input {
+            width: 100%;
+        }
+    }
+}
+</style>
