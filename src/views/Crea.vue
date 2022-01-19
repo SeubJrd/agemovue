a40345ac05f68ab96db43242455288d697f930bb
Sébastien Jourdain <sebj411@gmail.com>
2022-01-19 08:05:12 +0100

    ajout créa.vue pour template de création de chaussure

---
+++ src/views/Crea.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <input type="text" v-model="shoeName">
+        <button @click="submitShoe">Send</button>
+    </div>
+</template>
+ 
+<script>
+import axios from "axios";
+ 
+export default {
+    data() {
+        return {
+            shoeName: ''
+        }
+    },
+    methods: {
+        submitShoe() {
+ 
+            axios.post('https://lpwdapi.local/wp-json/wp/v2/shoes',
+                {
+                    "status": "publish",
+                    "title": this.shoeName
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${this.$store.state.user.authToken}`
+                    }
+                }).then(response => {
+ 
+                if (response.status === 200) {
+                    // FAITES CE QUI FAUT
+                }
+            })
+        }
+    }
+}
+</script>
